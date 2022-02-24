255d4026ea2ad346ca96ba2aa7e4748d4ca41401
Shrihan-28 <77265902+Shrihan-28@users.noreply.github.com>
2022-02-24 13:38:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function showNotes() {
     html += `
             <div class="noteCard my-2 mx-2 card" style="width: 18rem; onload="Timestamp()" ">
                     <div class="card-body">
-                        <h5 class="card-title">Title: ${element.title}</h5>
+                        <h5 class="card-title">${element.title}</h5>
                         <p class="card-text">Note: ${element.text}</p>
                         <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                         <hr>
